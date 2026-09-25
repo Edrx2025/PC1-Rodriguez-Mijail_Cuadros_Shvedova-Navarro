363b68390916fc8548431f7742c18b45e0502a68
MijailCSh <mijailc@protonmail.com>
2026-09-24 19:56:16 -0500

    otro commit

--- pregunta2/src/pregunta2/repositorio/RepositorioMatricula.java
+++ pregunta2/src/pregunta2/repositorio/RepositorioMatricula.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package pregunta2.repositorio;
 
-/**
- *
- * @author mijailc
- */
+import java.util.List;
+import pregunta2.Modelo.Matricula;
+
+
 public interface RepositorioMatricula {
-    
+    void guardar(Matricula m);
+    boolean existe(String codigoAlumno, String codigoCurso, String periodo);
+    List<Matricula> porPeriodo(String periodo);
+    List<Matricula> porAlumno(String codigoAlumno);
+    List<Matricula> porCurso(String codigoCurso);
 }
--- pregunta2/src/pregunta2/servicio/ServicioMatricula.java
+++ pregunta2/src/pregunta2/servicio/ServicioMatricula.java
@@ -1,5 +1,17 @@
 package pregunta2.servicio;
 
+import pregunta2.repositorio.RepositorioAlumno;
+import pregunta2.repositorio.RepositorioCurso;
+import pregunta2.repositorio.RepositorioMatricula;
+
 public class ServicioMatricula {
+    private final RepositorioMatricula repositorio;
+    private final RepositorioAlumno repositorioAlumno;
+    private final RepositorioCurso repositorioCurso;
+        public ServicioMatricula(RepositorioMatricula repositorio, RepositorioAlumno repositorioAlumno, RepositorioCurso repositorioCurso) {
+            this.repositorio = repositorio;
+            this.repositorioAlumno = repositorioAlumno;
+            this.repositorioCurso = repositorioCurso;
+        }
     
-}
+ }
